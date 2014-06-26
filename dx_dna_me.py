d9f7a3cd0c95f9c39e33e26b230ef06d3944340e
Ben Hitz <hitz@genome.stanford.edu>
2014-06-25 18:08:01 -0700

    rename applets

--- dx_dna_me.py
+++ dx_dna_me.py
@@ -9,6 +9,12 @@ import dxpy
 ENCODE_DNA_ME_PROJECT_NAME = 'Methylation Prototype'
 ''' This DNA Nexus project holds all the created applets and folders'''
 
+ENCODE_REFERENCES_PROJECT = 'Encode Reference Files'
+''' This DNA Nexus project holds Reference files (unaccessioned) used across several pipelines'''
+
+ENCODE_SNAPSHOT_PROJECT = 'ENCODE-SDSC-snapshot-20140505'
+''' This DNA Nexus project holds ENCFF files; should be replaced by a more permanent store '''
+
 REPLICATES_FOLDER = '/replicates'
 
 GENOME_REFERENCES = {
@@ -39,7 +45,6 @@ def get_args():
 
     ap.add_argument('-e', '--experiment',
                     help='ENCODED experiment accession',
-                    nargs='+',
                     required=True)
 
     ap.add_argument('-g', '--gender',
@@ -55,20 +60,20 @@ def get_args():
 
     return ap.parse_args()
 
-def find_reference_file_by_name(reference_name, applets_project_id):
+def find_reference_file_by_name(reference_name, project_name):
     '''Looks up a reference file by name in the project that holds common tools. From Joe Dale's code.'''
+    project = dxpy.find_one_project(name=project_name, name_mode='exact', return_handler=False)
     cached = '*'
-    if (reference_name, applets_project_id) not in REFERENCE_FILES:
+    if (reference_name, project['id']) not in REFERENCE_FILES:
         found = dxpy.find_one_data_object(classname="file", name=reference_name,
-                                          project=applets_project_id,
-                                          folder='/Reference Data',
+                                          project=project['id'],
                                           recurse=True,
                                           zero_ok=False, more_ok=False, return_handler=True)
-        REFERENCE_FILES[(reference_name, applets_project_id)] = found
+        REFERENCE_FILES[(reference_name, project['id'])] = found
         cached = ''
 
-    print cached + "Resolved %s to %s" % (reference_name, REFERENCE_FILES[(reference_name, applets_project_id)].get_id())
-    return dxpy.dxlink(REFERENCE_FILES[(reference_name, applets_project_id)])
+    print cached + "Resolved %s to %s" % (reference_name, REFERENCE_FILES[(reference_name, project['id'])].get_id())
+    return dxpy.dxlink(REFERENCE_FILES[(reference_name, project['id'])])
 
 def find_applet_by_name(applet_name, applets_project_id):
     '''Looks up an applet by name in the project that holds tools.  From Joe Dale's code.'''
@@ -84,7 +89,7 @@ def find_applet_by_name(applet_name, applets_project_id):
     return APPLETS[(applet_name, applets_project_id)]
 
 def get_project(project_name):
-    project = dxpy.find_projects(name=project_name, name_mode='glob', return_handler=True)
+    project = dxpy.find_projects(name=project_name, name_mode='glob', return_handler=False)
 
     project = [p for p in project]
     if len(project) < 1:
@@ -101,13 +106,13 @@ def get_project(project_name):
 def populate_workflow(wf, replicates, experiment, paired, gender, organism, applets_project_id):
     '''This function will populate the workflow for the methyl-seq Pipeline.'''
 
-    genome = find_reference_file_by_name(GENOME_REFERENCES[organism])
+    genome = find_reference_file_by_name(GENOME_REFERENCES[organism], ENCODE_REFERENCES_PROJECT)
     # TODO somethink like loop over analysis_steps in pipeline objects
     ### INDEX
     index_input = {
         'genome': genome
     }
-    stage_id = wf.add_stage(find_applet_by_name('index'), applets_project_id, stage_input=index_input, folder=experiment)
+    stage_id = wf.add_stage(find_applet_by_name('index', applets_project_id), stage_input=index_input, folder=experiment)
     index_output = dxpy.dxlink({
         'stage': stage_id,
         'outputField': 'meIndex'
@@ -116,30 +121,34 @@ def populate_workflow(wf, replicates, experiment, paired, gender, organism, appl
     trim_input = {
         'reads': replicates
     }
-    stage_id = wf.add_stage(find_applet_by_name('trim'), applets_project_id, stage_input=trim_input, folder=experiment)
-    trim_output = dxpy.dxlink({
-        'stage': stage_id,
-        'outputField': 'trimmed_reads'
-    })
+    if not paired:
+        stage_id = wf.add_stage(find_applet_by_name('trim-se', applets_project_id), stage_input=trim_input, folder=experiment)
+        trim_output = dxpy.dxlink({
+            'stage': stage_id,
+            'outputField': 'trimmed_reads'
+        })
 
-    ### MAP
-    map_input = {
-        'genome': genome,
-        'trimmed_reads': trim_output,
-        'meIndex': index_output
-    }
-    stage_id = wf.add_stage(find_applet_by_name('map'), applets_project_id, stage_input=map_input, folder=experiment)
-    map_output = dxpy.dxlink({
-        'stage': stage_id,
-        'outputField': 'mapped_files'
-    })
+        ### MAP
+        map_input = {
+            'genome': genome,
+            'trimmed_reads': trim_output,
+            'meIndex': index_output
+        }
+        stage_id = wf.add_stage(find_applet_by_name('map-se', applets_project_id), stage_input=map_input, folder=experiment)
+        map_output = dxpy.dxlink({
+            'stage': stage_id,
+            'outputField': 'mapped_files'
+        })
+    else:
+        print "Paired end not implemented"
+        exit(0)
 
     ### EXTRACT
     extract_input = {
         'genome': genome,
         'mapped_files': map_output
     }
-    stage_id = wf.add_stage(find_applet_by_name('extract'), applets_project_id, stage_input=extract_input, folder=experiment)
+    stage_id = wf.add_stage(find_applet_by_name('extract', applets_project_id), stage_input=extract_input, folder=experiment)
 
 
 
@@ -170,7 +179,7 @@ def resolve_applets_project():
         print 'Could not find 1 and only 1 project named {0}.'.format(ENCODE_DNA_ME_PROJECT_NAME)
         exit(0)
 
-    return dxpy.DXProject(project['id]'])
+    return dxpy.DXProject(project['id'])
 
 def main():
     args = get_args()
@@ -180,14 +189,15 @@ def main():
     #print project.keys()
     print 'Project: ' + project.describe()['name']
     #print project.keys()
-    project_folder = project_has_folder(project, args.experiment)
+    print 'Experiment to analyze: ' + args.experiment
+    project_folder = project_has_folder(project, '/'+args.experiment)
     if not project_folder:
-        project_folder = project.new_folder(args.experiment)
+        project_folder = project.new_folder('/'+args.experiment)
 
     #TODO get all replicate ids from encoded DB from ENCSR (args.experiment)
     #TODO error out if ENCSR not found, status not complete etc.
 
-    replicates_folder = project_folder+REPLICATES_FOLDER
+    replicates_folder = '/'+args.experiment+REPLICATES_FOLDER
     if not project_has_folder(project, replicates_folder):
         replicates = dxpy.find_data_objects(classname='file', name='*.fq.gz',
                                             name_mode='glob', project=project.get_id(),
@@ -209,7 +219,7 @@ def main():
                              description='The ENCODE Bismark pipeline for WGBS shotgun methylation analysis',
                              project=project.get_id())
 
-    populate_workflow(wf, replicates, args.experiment, paired, gender, organism, project['id'])
+    populate_workflow(wf, replicates, args.experiment, paired, gender, organism, project.id)
     #TODO - run the workflow automatically
     #TODO - export template workflows
 
