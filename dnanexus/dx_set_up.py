6de57134c700d27ec6c10afcf191fa6551bb7b6d
Ben Hitz <hitz@genome.stanford.edu>
2014-11-26 12:22:58 -0800

    wip

--- dnanexus/dx_set_up.py
+++ dnanexus/dx_set_up.py
@@ -10,6 +10,9 @@ import requests
 SERVER = 'https://www.encodeproject.org/'
 ASSAY_TYPE = 'whole genome bisulfite sequencing'
 HEADERS = {'content-type': 'application/json'}
+PROJECT_NAME = 'dna-me-pipeline'
+APPLETS = {}
+
 
 def find_applet_by_name(applet_name, applets_project_id):
     '''Looks up an applet by name in the project that holds tools.  From Joe Dale's code.'''
@@ -39,11 +42,21 @@ def get_args():
 
     return ap.parse_args()
 
+def resolve_project(project_name, level=None):
+    try:
+        project = dxpy.find_one_project(name=project_name, name_mode='exact',
+                                        level=level, return_handler=False)
+    except:
+        print 'Could not find 1 and only 1 project named %s; ' % format(project_name)
+        sys.exit(1)
+
+    return dxpy.DXProject(project['id'])
+
 def main():
     args = get_args()
 
     ## resolve projects
-    project = resolve_project(ENCODE_DNA_ME_PROJECT_NAME)
+    project = resolve_project(PROJECT_NAME)
     print 'Project: ' + project.describe()['name']
     pid =  project.get_id()
 
