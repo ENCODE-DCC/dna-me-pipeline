0a460fa7c2d4c24c5e9fb26e4f0007204acd3554
Ben Hitz <hitz@genome.stanford.edu>
2014-12-19 14:05:57 -0800

    add replicate to post

--- dnanexus/dnameSplashdown.py
+++ dnanexus/dnameSplashdown.py
@@ -405,6 +405,8 @@ def main():
             f_ob['lab'] = exp['lab']['@id']
             f_ob['award'] = exp['award']['@id']
             f_ob['assembly'] = mapping['genome']
+            ## temporary haxors until file display works
+            f_ob['replicate'] = mapping['replicate_id']
             f_ob['notes'] = json.dumps(dxencode.create_notes(dxFile, get_software()))
             print json.dumps(f_ob, sort_keys=True, indent=4, separators=(',',': '))
             if args.test:
