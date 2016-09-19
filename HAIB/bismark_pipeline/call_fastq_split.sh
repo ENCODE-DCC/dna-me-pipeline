2e17ce249e1128576152cf363489e6e717b20176
Surya Chhetri <chhetribsurya@gmail.com>
2016-09-19 16:22:19 -0500

    call_fastq_split.sh updated

--- HAIB/bismark_pipeline/call_fastq_split.sh
+++ HAIB/bismark_pipeline/call_fastq_split.sh
@@ -7,7 +7,7 @@ export RUN_PATH=`pwd`
 ### Iterates over each SL# libraries, and runs the job parallel on different nodes:
 for LIB in ${LIB_LIST}; do
 	export LIST=$LIB
-	export INPUT_DIR=/gpfs/gpfs1/myerslab/data/Libraries/$LIB
+	export INPUT_DIR=$INPUT_LOC/$LIB
 	JOB_NAME="Splitting of fastq"	
 	### Will take 4 cores of the HAIB morgan cluster for each library, submitting the jobs to different node instances 
 	bsub -We 24:00 -n 4  -R span[hosts=1] -J "$JOB_NAME" -o $OUTPUT_DIR/fastq_split.out $RUN_PATH/fastq_split.sh $LIB
